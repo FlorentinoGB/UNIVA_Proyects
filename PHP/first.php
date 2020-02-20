af18640121c7ca626673b371af2bf71b161dc9f6
FlorentinoGB <florentino.beltrang@gmail.com>
2020-02-19 22:05:13 -0600

    Learnin php lesson 1

---
+++ PHP/first.php
@@ -0,0 +1,60 @@
+<?php
+
+//One line comment
+
+/*
+More
+than
+one
+Line
+*/
+
+//Variables
+
+   // $firstName = "";
+
+    //$firstname = "";
+
+    $first_name = "Tino";
+    $last_name ="Beltán";
+
+    echo $first_name;
+    print $last_name;
+
+    $actualyear = 2020;
+    $startyear = 2016;
+    $spendedyears ="";
+
+    $echo "<hi>Year: $actualyear</h1>";
+    $echo "Start year: ".$startyear;
+    $spendedyears = $actualyear - $startyear;
+    $totalyears = 
+
+    print "<h3>Spended years: $spendedyears</h3>";
+
+    // Aritmetic operators
+
+    //Addition
+
+
+    //Sustraction
+    //Multiplication
+    //Division
+
+    // if
+    if ($spendedyears > 1) {
+        print "You are going ahead";
+    }
+        else
+        {
+            echo "Come on dude..";
+
+?>
+    <a href=""></a>
+
+<?php
+        }
+
+    
+
+?>
